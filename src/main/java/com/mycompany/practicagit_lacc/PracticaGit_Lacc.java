91939972d0abd16c014b91d45ede968cd02fa77d
Pomuch12 <Pomuch12@DESKTOP-6BF6VNA>
2026-03-03 07:39:31 -0600

    Faat: estructura inícial del proyecto

---
+++ src/main/java/com/mycompany/practicagit_lacc/PracticaGit_Lacc.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicagit_lacc;
+
+/**
+ *
+ * @author Pomuch12
+ */
+public class PracticaGit_Lacc {
+
+    public static void main(String[] args) {
+        System.out.println("---Sistema de gestion ");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
